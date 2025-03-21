02cefe7a326d4adbe221fc82369923858099e905
Ivan Harohau <olevel806@gmail.com>
2025-03-21 19:18:33 +0300

    added DataLoader class, fixed a few bugs

--- train_gpt2.py
+++ train_gpt2.py
@@ -93,7 +93,23 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-    def forward(self, idx):
+        #we want linear layer before classifier and token embeddings be the same tensor, as in original paper
+        self.transformer.wte.weight = self.lm_head.weight
+        #initialize params as in original code
+        self.apply(self._init_weights)
+
+
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+
+    def forward(self, idx, targets=None):
         B, T = idx.size()
         assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is {B}"
 
@@ -107,8 +123,12 @@ class GPT(nn.Module):
             x = block(x)
         #layernorm and classifier
         x = self.transformer.ln_f(x)
+
         logits = self.lm_head(x)
-        return logits
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
         
             
 
@@ -164,29 +184,90 @@ class GPT(nn.Module):
 
 
 #----------------------------------------------------------------------------------
+import tiktoken
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        self.current_position = 0
+    def next_batch(self):
+        B, T = self.B, self.T
+        #load the batch
+        buf = self.tokens[self.current_position : self.current_position + B*T + 1]
+        x = buf[:-1].view(B, T)
+        y = buf[1:].view(B, T)
+        #move current pos
+        self.current_position += B * T
+        #if we are out of bounds reset to zero
+        if self.current_position + B * T + 1 > len(self.tokens):
+            self.current_position = 0
+        return x, y
+#----------------------------------------------------------------------------------
+
+#autodetect the device
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    devise = "mps"
+print(f"using device {device}")
+
+train_loader = DataLoaderLite(4, 32)
+
+
+#get logits
+#model = GPT.from_pretrained('gpt2')
+model = GPT(GPTConfig())
+model.to(device)
+#logits, loss = model(x, y)
+
+#optimize
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    optimizer.zero_grad()
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    logits, loss = model(x,y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
+import sys; sys.exit(0)
+
+
+#prefix tokens
+#model = GPT.from_pretrained('gpt2')
+model = GPT(GPTConfig())
+model.eval()
+model.to(device)
 num_return_sequences = 5
 max_length = 30
 
-model = GPT.from_pretrained('gpt2')
-model.eval()
-model.to('cpu')
 
-#prefix tokens
 import tiktoken
 enc = tiktoken.get_encoding('gpt2')
 tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype=torch.long)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to('cpu')
+x = tokens.to(device)
 
-#generate x is (B, T) B=5, T=8
+#generate: x is (B, T) B=5, T=8
 #seed = 42
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 while x.size(1) < max_length:
     with torch.no_grad():
         logits = model(x)
-
+        #get probs prom last logits
         probs = F.softmax(logits[:, -1, :], dim=-1)
         #get top 50 most likely tokens (k = 50)
         topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
